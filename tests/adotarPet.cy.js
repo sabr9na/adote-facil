3b94f280d990c04ef91562c16f7ab1cd6903a015
Guroo8 <138990604+Guroo8@users.noreply.github.com>
2026-02-16 12:45:41 -0300

    Update adotarPet.cy.js

--- tests/adotarPet.cy.js
+++ tests/adotarPet.cy.js
@@ -7,7 +7,12 @@ describe('template spec', () => {
     cy.get(':nth-child(1) > .sc-66a75d78-2 > a > .sc-ea747762-0').click()
     cy.wait(200)
     cy.get('.sc-ea747762-0').click()
-    //cy.wait(200)
-    //cy.get('.sc-6ec77ae5-7').type('Ola, tenho interesse em adotar esse pet')
+    cy.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+    })
+    cy.get('.sc-6ec77ae5-7').type('Ola, tenho interesse em adotar esse pet').type('{enter}')
   })
+
 })
